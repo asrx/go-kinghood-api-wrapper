1b9ddc1c75abc19c04c735ebce87f122472e3f2b
Donovan.Xu <xudong0226@163.com>
2024-05-14 11:20:27 +0800

    addrType

--- src/Entry/Address.go
+++ src/Entry/Address.go
@@ -1,15 +1,15 @@
 package Entry
 
 type Address struct {
-	PostalCode	string  `json:"PostalCode"`
-	Name		string  `json:"Name,omitempty"`
-	Company		string  `json:"Company",omitempty`
-	Address1	string  `json:"Address1"`
-	Address2	string  `json:"Address2,omitempty"`
-	State		string  `json:"State"`
-	City		string  `json:"City"`
-	Country		string  `json:"Country"`
-	PhoneNumber	string  `json:"PhoneNumber"`
+	PostalCode  string `json:"PostalCode"`
+	Name        string `json:"Name"`
+	Company     string `json:"Company"`
+	Address1    string `json:"Address1"`
+	Address2    string `json:"Address2"`
+	State       string `json:"State"`
+	City        string `json:"City"`
+	Country     string `json:"Country"`
+	PhoneNumber string `json:"PhoneNumber"`
 	// 0:自动判断，1：商业，2：住宅
-	AddrType	uint  	`json:"AddrType,omitempty"`
+	AddrType uint `json:"AddrType"`
 }
