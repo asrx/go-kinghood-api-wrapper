a11976a832f2caec0cbf47066ea6ccbd5591b88c
Donovan.Xu <xudong0226@163.com>
2024-08-06 15:00:11 +0800

    add GetLabel2

--- src/Entry/LabelInfo.go
+++ src/Entry/LabelInfo.go
@@ -1,36 +1,40 @@
 package Entry
 
 type LabelInfo struct {
-	Code    string      `json:"Code"`
-	Message interface{} `json:"Message"`
-	Data    []struct {
-		ShipmentId            string      `json:"ShipmentId"`
-		LabelReady            bool        `json:"LabelReady"`
-		LabelUrl              *string     `json:"LabelUrl"`
-		MasterTrackingNumber  *string     `json:"MasterTrackingNumber"`
-		TrackingNumbers       []string    `json:"TrackingNumbers"`
-		MasterTrackingNumbers interface{} `json:"MasterTrackingNumbers"`
-		Parcels               []struct {
-			TrackingNumber string      `json:"TrackingNumber"`
-			Weight         float64     `json:"Weight"`
-			Length         float64     `json:"Length"`
-			Width          float64     `json:"Width"`
-			Height         float64     `json:"Height"`
-			Insure         float64     `json:"Insure"`
-			Picking        interface{} `json:"Picking"`
-			FbaRef         interface{} `json:"FbaRef"`
-			Quantity       int         `json:"Quantity"`
-			LabelUrl       interface{} `json:"LabelUrl"`
-			AdditionalFee  int         `json:"AdditionalFee"`
-		} `json:"Parcels"`
-		OrderNum            string      `json:"OrderNum"`
-		LabelStatus         int         `json:"LabelStatus"`
-		LabelStatusTxt      string      `json:"LabelStatusTxt"`
-		TN2                 interface{} `json:"TN2"`
-		Carrier             string      `json:"Carrier"`
-		CarrierServiceLevel string      `json:"CarrierServiceLevel"`
-		Weight              int         `json:"Weight"`
-		ShippingPrice       float64     `json:"ShippingPrice"`
-		ChargeDetail        interface{} `json:"ChargeDetail"`
-	} `json:"Data"`
+	Code    string       `json:"Code"`
+	Message interface{}  `json:"Message"`
+	Data    []*LabelData `json:"Data"`
 }
+
+type LabelData struct {
+	ShipmentId            string      `json:"ShipmentId"`
+	LabelReady            bool        `json:"LabelReady"`
+	LabelUrl              *string     `json:"LabelUrl"`
+	MasterTrackingNumber  *string     `json:"MasterTrackingNumber"`
+	TrackingNumbers       []string    `json:"TrackingNumbers"`
+	MasterTrackingNumbers interface{} `json:"MasterTrackingNumbers"`
+	Parcels               []*Parcels  `json:"Parcels"`
+	OrderNum              string      `json:"OrderNum"`
+	LabelStatus           int         `json:"LabelStatus"`
+	LabelStatusTxt        string      `json:"LabelStatusTxt"`
+	TN2                   interface{} `json:"TN2"`
+	Carrier               string      `json:"Carrier"`
+	CarrierServiceLevel   string      `json:"CarrierServiceLevel"`
+	Weight                int         `json:"Weight"`
+	ShippingPrice         float64     `json:"ShippingPrice"`
+	ChargeDetail          interface{} `json:"ChargeDetail"`
+}
+
+// Parcels struct {
+// TrackingNumber string      `json:"TrackingNumber"`
+// Weight         float64     `json:"Weight"`
+// Length         float64     `json:"Length"`
+// Width          float64     `json:"Width"`
+// Height         float64     `json:"Height"`
+// Insure         float64     `json:"Insure"`
+// Picking        interface{} `json:"Picking"`
+// FbaRef         interface{} `json:"FbaRef"`
+// Quantity       int         `json:"Quantity"`
+// LabelUrl       interface{} `json:"LabelUrl"`
+// AdditionalFee  int         `json:"AdditionalFee"`
+// }
